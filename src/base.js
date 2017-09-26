8078978123dca960a3b496bb0aeba67dddcf394c
Michael Kantor <michael@layer.com>
2017-09-26 13:39:56 -0700

    Version tweak

--- src/base.js
+++ src/base.js
@@ -565,7 +565,7 @@ layerUI.init = function init(settings) {
  *
  * @type {String}
  */
-layerUI.version = '3.0.20';
+layerUI.version = '3.0.0';
 
 const clientVersions = Layer.Client.version.split('.').map(value => Number(value));
 if (clientVersions[0] !== 3 && Layer.Client.version !== '3.1.1') {
